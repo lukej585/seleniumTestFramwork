fe83a3b7b7fa386d6d9be6a2a5e55e2f2845f746
Luke <gertrude585@gmail.com>
2016-05-14 22:56:37 +0100

    update to methods

--- Utility/BaseTest.java
+++ Utility/BaseTest.java
@@ -1,10 +1,39 @@
+import org.junit.BeforeClass;
+import org.openqa.selenium.*;
+import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * Created by Luke on 14/05/2016.
  */
-public class BaseTest {
+public abstract class BaseTest implements org.junit.BeforeClass{
+
+    public static WebDriver driver;
 
     public BaseTest(){
-        System.out.println(this.getClass().toString() + " invoked");
+        System.out.println(getClass().toString() + " invoked");
     }
+
+    @BeforeClass
+    public static void setup(){
+
+        driver = new FirefoxDriver();
+
+    }
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
+
+
+    //TODO: implement find that uses the driver
+    public WebElement findElement(By by){
+
+        //WebElement element =
+
+        return null;
+    }
+
+
 
 }
